0b4f466981a1cb12b2dbf530a1e49f9dde009b9c
aachwabs2022 <aa.chwabs2022@gmail.com>
2025-11-06 16:57:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 
-st.header('st.write')
+# st.header('st.write')
 
 # df = pd.DataFrame({
 #     'first column': [1, 2, 3, 4],
@@ -11,10 +11,21 @@ st.header('st.write')
 #      })
 # st.write('text above', df, 'text below')
 
-df = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns=['a', 'b', 'c'])
-c = alt.Chart(df).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
-)
-st.write(c)
+# df = pd.DataFrame(
+#     np.random.randn(200, 3),
+#     columns=['a', 'b', 'c'])
+# c = alt.Chart(df).mark_circle().encode(
+#     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+# )
+# st.write(c)
+
+from datetime import time, datetime
+
+st.header('st.slider')
+
+# Example 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
