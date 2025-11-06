17ba1daa46814ab16d0dfd9849f7a4b1ea375721
aachwabs2022 <aa.chwabs2022@gmail.com>
2025-11-06 17:10:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,11 +82,30 @@ st.header('st.slider')
 
 # Example 'MULTI SELECT BOX'
 
-st.header('st.multiselect')
+# st.header('st.multiselect')
 
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
+# options = st.multiselect(
+#      'What are your favorite colors',
+#      ['Green', 'Yellow', 'Red', 'Blue'],
+#      ['Yellow', 'Red'])
 
-st.write('You selected:', options)
+# st.write('You selected:', options)
+
+# Example 'CHECKBOX'
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
